e35c8b859c173867aecaabb593ad60fad62835dd
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-10-25 12:37:53 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -6,6 +6,7 @@
  */
 const sharp = require('sharp');
 const redirect = require('./redirect');
+const params= require('./params');
 
 const sharpStream = _ => sharp({ animated: !process.env.NO_ANIMATE, unlimited: true });
 const format= req.params.webp ? 'webp' : 'jpeg';
